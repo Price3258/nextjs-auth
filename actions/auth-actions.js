8a6fb97711ba4ecf3ddc24e2ef08c00ff19dc2df
김원태 <rladnjsxo123@naver.com>
2025-03-04 14:21:50 +0900

    212 유저 생성하기

--- actions/auth-actions.js
+++ actions/auth-actions.js
@@ -1,5 +1,9 @@
 "use server";
 
+import { hashUserPassword } from "@/lib/hash";
+import { createUser } from "@/lib/user";
+import { redirect } from "next/navigation";
+
 export async function signup(prevState, formData) {
   const email = formData.get("email");
   const password = formData.get("password");
@@ -17,5 +21,19 @@ export async function signup(prevState, formData) {
     return { errors };
   }
 
-  // store it in the database
+  const hashedPassword = hashUserPassword(password);
+  try {
+    createUser(email, hashedPassword);
+  } catch (error) {
+    if (error.code === "SQLITE_CONSTRAINT_UNIQUE") {
+      return {
+        errors: {
+          email: "already existing email",
+        },
+      };
+    }
+    throw error;
+  }
+
+  redirect("/training");
 }
